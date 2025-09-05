5395b2dbe992c4a829a8bfcf6e8621ea72eac6ea
Thabo Matjuda <tl.matjuda@gmail.com>
2025-09-05 13:31:29 +0200

    chore(qabase-docs): Added Social Card to the QA Base Docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/qabase-docs-social-card.png',
     navbar: {
       title: 'QA Base',
       // logo: {
